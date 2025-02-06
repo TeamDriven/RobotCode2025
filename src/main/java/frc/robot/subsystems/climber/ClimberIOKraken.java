2c8d04c8e313daacef464c28e49dc69e85876df2
Christian Brock <Christiansbrock@gmail.com>
2025-02-05 20:08:04 -0600

    fixing inverted

--- src/main/java/frc/robot/subsystems/climber/ClimberIOKraken.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIOKraken.java
@@ -21,7 +21,7 @@ public class ClimberIOKraken implements ClimberIO {
     motorFactory = new MotorFactory("climber", tMotorID, bMotorID);
 
     motorFactory.setBrakeMode(true);
-    motorFactory.setInverted(false);
+    motorFactory.setInverted(false, false);
     motorFactory.setCurrentLimits(80);
     motorFactory.setVoltageLimits(12);
 
