40e06b128a7e1bf7a256d178e0e8b009d03c7d3d
Christian Brock <96064635+ChristianBrock3434@users.noreply.github.com>
2025-07-09 19:00:52 -0500

    added pose estimation

--- src/main/java/frc/robot/subsystems/vision/VisionIOPhotonVision.java
+++ src/main/java/frc/robot/subsystems/vision/VisionIOPhotonVision.java
@@ -1,27 +1,34 @@
 package frc.robot.subsystems.vision;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
-import frc.robot.LimelightHelpers;
 
 public class VisionIOPhotonVision implements VisionIO {
     PhotonCamera camera;
-    public static final AprilTagFieldLayout kTagLayout =
-                AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
-    public static final Transform3d kRobotToCam =
-                new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0, 0, 0));
+    public static final AprilTagFieldLayout kTagLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+    public static final Transform3d kRobotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5),
+            new Rotation3d(0, 0, 0));
+    private final PhotonPoseEstimator photonEstimator;
 
     public VisionIOPhotonVision(String name) {
         camera = new PhotonCamera(name);
+        photonEstimator = new PhotonPoseEstimator(kTagLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, kRobotToCam);
     }
 
     @Override
@@ -29,56 +36,46 @@ public class VisionIOPhotonVision implements VisionIO {
         var result = camera.getLatestResult();
         List<PhotonTrackedTarget> targets = result.getTargets();
 
-        if (result == null) {
-            inputs.avgTagArea = 0;
-            inputs.avgTagDist = 0;
-            inputs.latency = 0;
-            inputs.pose = new Pose2d();
-            inputs.tagCount = 0;
-            inputs.timestampSeconds = 0;
-            return;
-        }
-
         double totalArea = 0;
         double totalDist = 0;
         for (PhotonTrackedTarget target : targets) {
-            totalArea += target.getArea(); 
+            totalArea += target.getArea();
             totalDist += target.bestCameraToTarget.getTranslation().getNorm();
         }
 
+        
         inputs.avgTagArea = totalArea / targets.size();
         inputs.avgTagDist = totalDist / targets.size();
         inputs.latency = 0;
-        inputs.pose = estimate.pose;
+        inputs.pose = estimatedPose2dAverage();
         inputs.tagCount = targets.size();
         inputs.timestampSeconds = result.getTimestampSeconds();
     }
 
     @Override
-    public void setRobotOrientation(double yaw) {
-        LimelightHelpers.SetRobotOrientation(LIMEIGHT_NAME, yaw, 0, 0, 0, 0, 0);
-    }
-
-    @Override
     public void setPipeline(int mode) {
-        LimelightHelpers.setPipelineIndex(LIMEIGHT_NAME, mode);
+        camera.setPipelineIndex(mode);
     }
 
-    @Override
-    public void setLights(int mode) {
-        switch (mode) {
-            case 0:
-                LimelightHelpers.setLEDMode_PipelineControl(LIMEIGHT_NAME);
-                break;
-            case 1:
-                LimelightHelpers.setLEDMode_ForceOff(LIMEIGHT_NAME);
-                break;
-            case 2:
-                LimelightHelpers.setLEDMode_ForceBlink(LIMEIGHT_NAME);
-                break;
-            case 3:
-                LimelightHelpers.setLEDMode_ForceOn(LIMEIGHT_NAME);
-                break;
+    private Pose2d estimatedPose2dAverage() {
+        double size = camera.getAllUnreadResults().size();
+        double sumX = 0;
+        double sumY = 0;
+        double sumRadians = 0;
+        Optional<EstimatedRobotPose> visionEst = Optional.empty();
+        // If a valid pose estimate is available
+        if (visionEst.isPresent()) {
+            for (var change : camera.getAllUnreadResults()) {
+                visionEst = photonEstimator.update(change);
+                EstimatedRobotPose est = visionEst.get();
+                // Get the estimated Pose3d of the robot
+                Pose3d estimatedPose3d = est.estimatedPose; 
+                Pose2d estimatedPose2d = estimatedPose3d.toPose2d();
+                sumX += estimatedPose2d.getX();
+                sumY += estimatedPose2d.getY();
+                sumRadians += estimatedPose2d.getRotation().getRadians();
+            }   
         }
+        return new Pose2d(new Translation2d(sumX/size,sumY/size), new Rotation2d(sumRadians/size));
     }
 }
